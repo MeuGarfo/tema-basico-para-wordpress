8fc6e20af549a76617a3cd0fcc982bb32fca4500
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-07 17:54:50 -0300

    M  single.php

--- single.php
+++ single.php
@@ -4,13 +4,21 @@
     <title><?php the_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <link rel="apple-touch-icon-precomposed" sizes="410x410" href="<?php bloginfo('template_url');?>/screenshot.jpg">
-    <link rel="shortcut icon" href="<?php bloginfo('template_url');?>/screenshot.jpg">
-    <meta property="og:image" content="<?php bloginfo('template_url');?>/screenshot.jpg">    
+    <link rel="shortcut icon" href="<?php bloginfo('template_url');?>/screenshot.jpg"> 
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@aicouto" />
     <meta name="twitter:title" content="<?php the_title(); ?>" />
     <meta name="twitter:description" content="" />
-    <meta name="twitter:image" content="<?php bloginfo('template_url');?>/screenshot.jpg" />
+    <?php $url=wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); if($url){
+    ?><meta name="twitter:image" content="<?php print $url;?>" /> 
+    <meta property="og:image" content="<?php print $url;?>">          
+    <?php
+    }else{
+    ?><meta name="twitter:image" content="<?php bloginfo('template_url');?>/screenshot.jpg" />
+    <meta property="og:image" content="<?php bloginfo('template_url');?>/screenshot.jpg">       
+    <?php
+    }
+    ?>
 </head>
 <body>
     <?php
