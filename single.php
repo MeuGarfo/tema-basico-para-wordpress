614f3b95e22068b1ee4835cf1340592dc25215d6
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-22 16:31:42 -0300

    M  single.php

--- single.php
+++ single.php
@@ -44,14 +44,8 @@
                         print '<div class="text-justify">';
                         the_content();
                         print '</div>';
-
-						// If comments are open or we have at least one comment, load up the comment template
-						if (comments_open() || '0' != get_comments_number()) {
-						    print '<hr>';
-							comments_template();
-						}
-
-                        
+					    print '<hr>';
+						comments_template();
                     }
                 }
                 ?>
