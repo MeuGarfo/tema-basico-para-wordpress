c44544c7b0cfcf1c73e41bd7181b7edfdcff3040
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-22 16:11:22 -0300

    M  single.php

--- single.php
+++ single.php
@@ -47,6 +47,7 @@
 
 						// If comments are open or we have at least one comment, load up the comment template
 						if (comments_open() || '0' != get_comments_number()) {
+						    print '<hr>';
 							comments_template();
 						}
 
